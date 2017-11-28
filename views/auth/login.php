5e2a1fd8959291f1948e7f0feff513589b2dd69e
andriih <andrii.hnatyshyn@gmail.com>
2017-11-28 22:05:51 +0200

    fb authentification problem

--- views/auth/login.php
+++ views/auth/login.php
@@ -49,5 +49,52 @@ $this->params['breadcrumbs'][] = $this->title;
                 To modify the username/password, please check out the code <code>app\models\User::$users</code>.
             </div></div>
         </div>
+
+        <div class="col-md-2">
+            <script>
+                window.fbAsyncInit = function() {
+                    FB.init({
+                        appId      : '1818892128184744',
+                        cookie     : true,
+                        xfbml      : true,
+                        version    : 'v2.11'
+                    });
+
+                    FB.getLoginStatus(function(response) {
+                        statusChangeCallback(response);
+                    });
+
+                };
+
+                (function(d, s, id){
+                    var js, fjs = d.getElementsByTagName(s)[0];
+                    if (d.getElementById(id)) {return;}
+                    js = d.createElement(s); js.id = id;
+                    js.src = "https://connect.facebook.net/en_US/sdk.js";
+                    fjs.parentNode.insertBefore(js, fjs);
+                }(document, 'script', 'facebook-jssdk'));
+
+                function statusChangeCallback(response){
+                    if(response.status === 'connected'){
+                        console.log('Logged in!');
+                    }else{
+                        console.log('Not Connected');
+                    }
+                }
+
+                function checkLoginState() {
+                    FB.getLoginStatus(function(response) {
+                        statusChangeCallback(response);
+                    });
+                }
+            </script>
+
+            <fb:login-button
+                    scope="public_profile,email"
+                    onlogin="checkLoginState();">
+            </fb:login-button>
+
+
+        </div>
     </div>
 </div>
